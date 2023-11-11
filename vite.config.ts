1e66cf65df2f77fd2608bd850e8602a1e4fc4873
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2023-11-11 16:09:31 -0300

    add component Header

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // Importação do módulo 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
